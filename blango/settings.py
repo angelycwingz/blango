97a07db1f8901ee8db55ebe7a2d1fb582e89f6f7
Aditya Tandon <aruntandon.tandon007@gmail.com>
2024-05-08 13:43:48 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from configurations import Configuration
 from configurations import values
+from datetime import timedelta
 import dj_database_url
 
 class Dev(Configuration):
@@ -222,6 +223,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication",
         ],
         "DEFAULT_PERMISSION_CLASSES": [
           "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -254,6 +256,11 @@ class Dev(Configuration):
             "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
             "Basic": {"type": "basic"},
         }
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
     INTERNAL_IPS=["192.168.10.226"]
