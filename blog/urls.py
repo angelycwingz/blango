5090750634b9aa3553a40add5e4d7717a07266a0
Aditya Tandon <aruntandon.tandon007@gmail.com>
2024-05-14 11:02:00 +0000

    Finish Javascript intro and fundamentals

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, post_detail
+from .views import index, post_detail, post_table
 
 urlpatterns=[
   path("", index),
   path("post/<slug>", post_detail, name="blog-post-detail"),
+  path("post-table/", post_table, name="blog-post-table"),
 ]
