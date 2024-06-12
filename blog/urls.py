feac6f5a76fdc0d90bd42ff92e597aa2dc9e21ab
Aditya Tandon <aruntandon.tandon007@gmail.com>
2024-06-12 13:45:26 +0000

    Finish fetch and React hooks

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from .views import index, post_detail, post_table
 
 urlpatterns=[
   path("", index),
-  path("post/<slug>", post_detail, name="blog-post-detail"),
+  path("post/<slug>/", post_detail, name="blog-post-detail"),
   path("post-table/", post_table, name="blog-post-table"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ import logging
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.urls import reverse
 from blog.models import Post
 from blog.forms import CommentForm
 
@@ -39,7 +40,7 @@ def post_detail(request, slug):
  return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
 
 def post_table(request):
-  return render(request, "blog/post-table.html")
+  return render(request, "blog/post-table.html", {"post_list_url": reverse("post-list")})
 
 def get_ip(request):
   from django.http import HttpResponse
